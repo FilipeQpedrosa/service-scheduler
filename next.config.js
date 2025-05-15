76f07fae68a31f827ff0c0a1c312abb79d1246de
Filipe Pedrosa <ext.filipe.pedrosa@opptm2gt0xvq6lw.lan>
2025-05-15 21:43:24 +0100

    Update Next.js configuration for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ const nextConfig = {
     CUSTOMER_PORT: 3000,
     STAFF_PORT: 4000,
   },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // Staff portal routes
+        {
+          source: '/staff/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'staff.:hostname',
+            },
+          ],
+          destination: '/:path*',
+        },
+      ],
+    }
+  },
 }
 
 module.exports = nextConfig 
