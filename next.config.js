d1e131b4efb3cb5ca5def1ada3acf7472f34cbb1
Filipe Pedrosa <ext.filipe.pedrosa@opptm2gt0xvq6lw.lan>
2025-05-16 08:38:03 +0100

    Simplify Next.js config for initial deployment

--- next.config.js
+++ next.config.js
@@ -3,24 +3,7 @@ const nextConfig = {
   env: {
     CUSTOMER_PORT: 3000,
     STAFF_PORT: 4000,
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        // Staff portal routes
-        {
-          source: '/staff/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'staff.:hostname',
-            },
-          ],
-          destination: '/:path*',
-        },
-      ],
-    }
-  },
+  }
 }
 
 module.exports = nextConfig 
